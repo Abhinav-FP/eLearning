0aebfab1f82bebe61a4cdfed4d8f16240b6c6e81
Abhinav-FP <a.mathur@futureprofilez.com>
2025-04-15 13:06:17 +0530

    Created teachers page

--- src/pages/student/lessons/index.jsx
+++ src/pages/student/lessons/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import StudentLayout from "../Common/StudentLayout";
+import Image from "next/image";
 
 export default function Index() {
   const [tab, setTab] = useState("upcoming");
@@ -43,23 +44,29 @@ export default function Index() {
               <p className="text-[#CC2828] font-semibold">
                 {lesson.date}
               </p>
-              <p className="text-sm text-gray-600">{lesson.time}</p>
-              <p className="text-sm text-gray-600">{lesson.duration}</p>
+              <p className="text-sm text-gray-600">{lesson?.time || ""}</p>
+              <p className="text-sm text-gray-600">{lesson?.duration || ""}</p>
             </div>
               <div
                 className="bg-white rounded-xl shadow-sm p-4 flex items-center justify-between transition border-red-100 border-1"
               >
                   <div className="flex items-center space-x-3 mt-2">
-                    <div className="w-10 h-10 rounded-full bg-green-300" />
+                  <Image
+                    src="/profile.png"
+                    alt="Profile"
+                    className="w-10 h-10 rounded-full object-cover"
+                    height={40}
+                    width={40}
+                  />
                     <div>
                       <p className="text-sm font-medium text-gray-800">
-                        {lesson.teacher}
+                        {lesson?.teacher || ""}
                       </p>
                       <p className="text-xs text-gray-500">Teacher</p>
                     </div>
                   </div>
                 <div className="flex flex-col sm:flex-row gap-2">
-                  <button className="px-2 sm:px-10 py-2 text-[#CC2828] border border-[#CC2828] rounded-md text-xs sm:text-sm hover:bg-red-100 cursor-pointer">
+                  <button className="px-2 sm:px-10 py-2 text-[#CC2828] border border-[#CC2828] rounded-md text-xs sm:text-sm hover:bg-[#CC2828] hover:text-white cursor-pointer">
                     Reschedule
                   </button>
                   <button className="px-2 sm:px-10 py-2 bg-[#CC2828] text-white rounded-md text-xs sm:text-sm hover:bg-white hover:text-[#CC2828] border border-[#CC2828] cursor-pointer">

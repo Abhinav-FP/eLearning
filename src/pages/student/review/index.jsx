9e685233a1195a6305d4d0171a79087f6f210fb1
sunil-sainiibs37 <sunil.saini@internetbusinesssolutionsindia.com>
2025-04-19 17:24:47 +0530

    teacher detail pages design

--- src/pages/student/review/index.jsx
+++ src/pages/student/review/index.jsx
@@ -19,22 +19,22 @@ export default function ReviewsList() {
   }, [])
   return (
     <StudentLayout page={"Settings"}>
-      <div className="bg-white rounded-[10px] lg:rounded-[20px] p-5">
-        <h2 className="text-lg font-semibold text-red-600 mb-4">Recent Reviews</h2>
-        <div className="p-4">
+      <div className="p-5 lg:p-[30px] "> 
+        <h2 className="text-xl lg:text-2xl font-bold text-[#CC2828] tracking-[-0.04em] font-inter">Recent Reviews</h2>
+        <div className="mt-5 lg:mt-6 space-y-4 lg:space-5">
           {reviews.map((review, index) => (
-            <div className="relative bg-[#F6F7F7] rounded-lg shadow p-4 mb-4">
+            <div className="relative bg-[#F6F7F7] rounded-[10px] p-4 lg:p-5">
               <div className="flex justify-between items-start">
-                <h3 className="text-sm font-semibold text-red-600"> Review on Lesson –{review?.lessonId?.title}</h3>
+                <h3 className="text-base font-semibold text-[#CC2828] -tracking-[0.04em] font-inter" > Review on Lesson –{review?.lessonId?.title}</h3>
               </div>
-              <p className="text-sm text-gray-800 mt-2">{review?.description}</p>
-              <div className="flex justify-between items-center mt-4">
-                <div className="flex text-yellow-500 space-x-1">
+              <p className="text-sm font-medium font-inter text-black mt-1 -tracking-[0.04em]">{review?.description}</p>
+              <div className="flex justify-between items-center mt-2">
+                <div className="flex text-[#E4B750] space-x-1">
                   {Array.from({ length: review?.rating }).map((_, i) => (
                     <FaStar key={i} size={16} fill="currentColor" />
                   ))}
                 </div>
-                <span className="text-xs text-red-600">{moment(review?.updatedAt || review?.createdAt).format("MMMM DD, YYYY")}</span>
+                <span className="text-sm text-[#CC2828] font-inter font-medium -tracking-[0.04em]">{moment(review?.updatedAt || review?.createdAt).format("MMMM DD, YYYY")}</span>
               </div>
             </div>
           ))}

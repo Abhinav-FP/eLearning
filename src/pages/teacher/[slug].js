96c75ec8173caa789145aa564b593ab8b2d7b98c
Abhinav-FP <a.mathur@futureprofilez.com>
2025-05-05 10:16:08 +0530

    Updated code

--- src/pages/teacher/[slug].js
+++ src/pages/teacher/[slug].js
@@ -125,7 +125,7 @@ export default function Index() {
             <Calendar Availability={content} setIsPopupOpen={setIsPopupOpen} usedInPopup={false}/>
           </div>
         </div>
-        <Testimonial />
+        <Testimonial/>
         <BookingPopup
           isOpen={isPopupOpen}
           onClose={closePopup}

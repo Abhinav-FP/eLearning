310b8863c58edbc25d53e894b8701263e86c2f96
Abhinav-FP <a.mathur@futureprofilez.com>
2025-05-08 12:17:48 +0530

    Updated register page

--- src/pages/student/register/index.jsx
+++ src/pages/student/register/index.jsx
@@ -14,6 +14,8 @@ export default function Index() {
     const [loading, setLoading] = useState(false);
     const [passwordFocused, setPasswordFocused] = useState(false);
     const [isPasswordValid, setIsPasswordValid] = useState(true);
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     const [data, setData] = useState({
         name: "",
@@ -103,9 +105,18 @@ export default function Index() {
                 </div>
 
                 {/* Heading */}
-                <h2 className="text-center text-red-600 text-2xl font-semibold mb-8">
-                    REGISTER
+                <h2 className="text-center text-red-600 text-2xl font-semibold mb-2 uppercase">
+                    REGISTER as a student
                 </h2>
+                <p className="text-center text-sm text-black mb-8">
+                    Want to register as a Teacher instead?{" "}
+                    <Link
+                        href="/teacher-dashboard/register"
+                        className="inline-block text-blue-600 font-medium hover:underline hover:text-blue-800 transition duration-200"
+                    >
+                        Click here
+                    </Link>
+                </p>
 
                 {/* Form Fields */}
                 <form onSubmit={handleSubmit}>
@@ -163,36 +174,54 @@ export default function Index() {
                                 required
                             />
                         </div>
-                        <div className='w-full md:w-6/12 px-2.5 mb-5'>
-                            <label className="block text-base font-medium text-[#727272] tracking-[-0.06em] mb-1">Password</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    value={data.password}
-                                    onChange={handleChange}
-                                    onFocus={() => setPasswordFocused(true)}
-                                    onBlur={() => setPasswordFocused(false)}
-                                    className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
-                                    required
-                                />
-                                {passwordFocused && !isPasswordValid && (
+                        <div className="w-full md:w-6/12 px-2.5 mb-5 relative">
+                            <label className="block text-base font-semibold text-[#727272] tracking-[-0.06em] mb-1">
+                                Password
+                            </label>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                                value={data.password}
+                                onChange={handleChange}
+                                onFocus={() => setPasswordFocused(true)}
+                                onBlur={() => setPasswordFocused(false)}
+                                className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
+                                required
+                            />
+                            <div
+                                className="absolute top-9 right-5 cursor-pointer text-gray-600"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <IoEyeOff size={24} /> : <IoEye size={24} />}
+                            </div>
+                            {passwordFocused && !isPasswordValid && (
                                 <p className="text-red-600 text-sm mt-1">
-                                    Password must include at least one uppercase letter, one lowercase letter, a number, a special character, and be at least 8 characters long.
+                                Password must include at least one uppercase letter, one lowercase letter, a number, a special character, and be at least 8 characters long.
                                 </p>
                             )}
                         </div>
-                        <div className='w-full md:w-6/12 px-2.5 mb-5'>
-                            <label className="block text-base font-medium text-[#727272] tracking-[-0.06em] mb-1">Confirm Password</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    name="confirm_password"
-                                    value={data.confirm_password}
-                                    onChange={handleChange}
-                                    className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
-                                    required
-                                />
+                        <div className='w-full md:w-6/12 px-2.5 mb-5 relative'>
+                            <label className="block text-base font-semibold text-[#727272] tracking-[-0.06em] mb-1">
+                                Confirm Password
+                            </label>
+                            <input
+                                type={showConfirmPassword ? "text" : "password"}
+                                id="confirm_password"
+                                name="confirm_password"
+                                placeholder="Confirm Password"
+                                value={data?.confirm_password}
+                                onChange={handleChange}
+                                className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
+                                required
+                            />
+                            <div
+                                className="absolute top-9 right-5 cursor-pointer text-gray-600"
+                                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                            >
+                                {showConfirmPassword ? <IoEyeOff size={24} /> : <IoEye size={24} /> }
+                            </div>
                         </div>
                         {/* Gender */}
                         {/* <select
@@ -232,18 +261,14 @@ export default function Index() {
                                 <button
                                     type="submit"
                                     disabled={loading} //
-                                    className="w-full py-[15px] bg-[#CC2828] hover:bg-[#ad0e0e] cursor-pointer text-white py-2 rounded-md font-semibold transition"
+                                    className="w-full py-[15px] bg-[#CC2828] hover:bg-[#a11f1f] cursor-pointer text-white py-2 rounded-md font-semibold transition"
                                 >
                                     {loading ? "Loading.." : "Sign Up"} {/* Fixed typo */}
                                 </button>
                             </div>
                         </div>
-
-
                     </div>
-
                 </form>
-
 
                 {/* Login Redirect */}
                 <p className="text-center text-base text-[#727272] mt-6 lg:mt-12 tracking-[-0.03em] font-medium">
--- src/pages/teacher-dashboard/register/index.jsx
+++ src/pages/teacher-dashboard/register/index.jsx
@@ -14,6 +14,8 @@ export default function Index() {
     const [loading, setLoading] = useState(false);
     const [passwordFocused, setPasswordFocused] = useState(false);
     const [isPasswordValid, setIsPasswordValid] = useState(true);
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
     const [data, setData] = useState({
         name: "",
@@ -105,15 +107,24 @@ export default function Index() {
                 </div>
 
                 {/* Heading */}
-                <h2 className="text-center uppercase text-red-600 text-2xl font-semibold mb-8">
-                   Register as a Teacher
+                <h2 className="text-center text-red-600 text-2xl font-semibold mb-2 uppercase">
+                    REGISTER as a Teacher
                 </h2>
+                <p className="text-center text-sm text-black mb-8">
+                    Want to register as a Student instead?{" "}
+                    <Link
+                        href="/student/register"
+                        className="inline-block text-blue-600 font-medium hover:underline hover:text-blue-800 transition duration-200"
+                    >
+                        Click here
+                    </Link>
+                </p>
 
                 {/* Form Fields */}
                 <form onSubmit={handleSubmit}>
                     <div className='flex flex-wrap -mx-2.5 justify-center'>
                         <div className='w-full md:w-6/12 px-2.5 mb-5'>
-                            <label className="block text-base font-medium text-[#727272] tracking-[-0.06em] mb-1">Name</label>
+                            <label className="block text-base font-semibold text-[#727272] tracking-[-0.06em] mb-1">Name</label>
                             <input
                                 value={data?.name}
                                 onChange={handleChange}
@@ -125,7 +136,7 @@ export default function Index() {
                             />
                         </div>
                         <div className='w-full md:w-6/12 px-2.5 mb-5'>
-                            <label className="block text-base font-medium text-[#727272] tracking-[-0.06em] mb-1">Time-Zone</label>
+                            <label className="block text-base font-semibold text-[#727272] tracking-[-0.06em] mb-1">Time-Zone</label>
                             {/* Time-Zone */}
                             <select className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
                                 onChange={handleChange}
@@ -142,7 +153,7 @@ export default function Index() {
                             </select>
                         </div>
                         <div className='w-full md:w-6/12 px-2.5 mb-5'>
-                            <label className="block text-base font-medium text-[#727272] tracking-[-0.06em] mb-1">Email</label>
+                            <label className="block text-base font-semibold text-[#727272] tracking-[-0.06em] mb-1">Email</label>
                             <input
                                 value={data?.email}
                                 onChange={handleChange}
@@ -155,7 +166,7 @@ export default function Index() {
                             />
                         </div>
                         <div className='w-full md:w-6/12 px-2.5 mb-5'>
-                            <label className="block text-base font-medium text-[#727272] tracking-[-0.06em] mb-1">Email</label>
+                            <label className="block text-base font-semibold text-[#727272] tracking-[-0.06em] mb-1">Email</label>
                             <input
                                 value={data?.confirm_email}
                                 onChange={handleChange}
@@ -166,37 +177,56 @@ export default function Index() {
                                 required
                             />
                         </div>
-                        <div className='w-full md:w-6/12 px-2.5 mb-5'>
-                            <label className="block text-base font-medium text-[#727272] tracking-[-0.06em] mb-1">Password</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    value={data.password}
-                                    onChange={handleChange}
-                                    onFocus={() => setPasswordFocused(true)}
-                                    onBlur={() => setPasswordFocused(false)}
-                                    className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
-                                    required
-                                />
-                                {passwordFocused && !isPasswordValid && (
+                        <div className="w-full md:w-6/12 px-2.5 mb-5 relative">
+                            <label className="block text-base font-semibold text-[#727272] tracking-[-0.06em] mb-1">
+                                Password
+                            </label>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="password"
+                                name="password"
+                                placeholder="Password"
+                                value={data.password}
+                                onChange={handleChange}
+                                onFocus={() => setPasswordFocused(true)}
+                                onBlur={() => setPasswordFocused(false)}
+                                className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
+                                required
+                            />
+                            <div
+                                className="absolute top-9 right-5 cursor-pointer text-gray-600"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? <IoEyeOff size={24} /> : <IoEye size={24} />}
+                            </div>
+                            {passwordFocused && !isPasswordValid && (
                                 <p className="text-red-600 text-sm mt-1">
-                                    Password must include at least one uppercase letter, one lowercase letter, a number, a special character, and be at least 8 characters long.
+                                Password must include at least one uppercase letter, one lowercase letter, a number, a special character, and be at least 8 characters long.
                                 </p>
                             )}
                         </div>
-                        <div className='w-full md:w-6/12 px-2.5 mb-5'>
-                            <label className="block text-base font-medium text-[#727272] tracking-[-0.06em] mb-1">Confirm Password</label>
-                                <input
-                                    type="password"
-                                    id="password"
-                                    name="confirm_password"
-                                    value={data.confirm_password}
-                                    onChange={handleChange}
-                                    className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
-                                    required
-                                />
+                        <div className='w-full md:w-6/12 px-2.5 mb-5 relative'>
+                            <label className="block text-base font-semibold text-[#727272] tracking-[-0.06em] mb-1">
+                                Confirm Password
+                            </label>
+                            <input
+                                type={showConfirmPassword ? "text" : "password"}
+                                id="confirm_password"
+                                name="confirm_password"
+                                placeholder="Confirm Password"
+                                value={data?.confirm_password}
+                                onChange={handleChange}
+                                className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
+                                required
+                            />
+                            <div
+                                className="absolute top-9 right-5 cursor-pointer text-gray-600"
+                                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                            >
+                                {showConfirmPassword ? <IoEyeOff size={24} /> : <IoEye size={24} /> }
+                            </div>
                         </div>
+
                         {/* Gender */}
                         {/* <select
                                 className="w-full px-4 lg:px-5 py-2 border h-[48px] lg:h-[56px] border-[#F4F6F8] rounded-[6px] lg:rounded-[10px] bg-[#F4F6F8] focus:outline-none focus:ring-1 focus:ring-[#c9c9c9]"
@@ -235,19 +265,14 @@ export default function Index() {
                                 <button
                                     type="submit"
                                     disabled={loading} //
-                                    className="w-full py-[15px] bg-[#CC2828] hover:bg-[#ad0e0e] cursor-pointer text-white py-2 rounded-md font-semibold transition"
+                                    className="w-full py-[15px] bg-[#CC2828] hover:bg-[#a11f1f] cursor-pointer text-white py-2 rounded-md font-semibold transition"
                                 >
                                     {loading ? "Loading.." : "Sign Up"} {/* Fixed typo */}
                                 </button>
                             </div>
                         </div>
-
-
                     </div>
-
                 </form>
-
-
                 {/* Login Redirect */}
                 <p className="text-center text-base text-[#727272] mt-6 lg:mt-12 tracking-[-0.03em] font-medium">
                     Already have an account?{" "}

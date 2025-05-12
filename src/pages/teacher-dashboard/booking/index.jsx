f04ef88196ee01d3da529765871d355bf3dee535
Abhinav-FP <a.mathur@futureprofilez.com>
2025-05-12 11:20:13 +0530

    Added booking page

---
+++ src/pages/teacher-dashboard/booking/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TeacherLayout from '../Common/TeacherLayout'
+
+export default function Index() {
+  return (
+    <TeacherLayout>
+        index
+    </TeacherLayout>
+  )
+}

ecdbd517ce72058e85699cb14c02bee5442d83d4
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-06-06 17:57:58 +0530

    manage chnagement

--- src/pages/find-teacher/Teacher.jsx
+++ src/pages/find-teacher/Teacher.jsx
@@ -17,7 +17,32 @@ export default function Teacher({ teacherData, loading }) {
                 <div className="text-center mb-8 lg:mb-10">
                     <Image className="inline-block" src={LineImg} alt="icon" />
                 </div>
-                {loading ?
+                  {loading ?
+                    <BookLoader />
+                    :
+                    <div className="flex flex-wrap justify-center -mx-2.5">
+                        {
+                            teacherData && teacherData?.map((item, i) => (
+                                <Link href={`/teacher/${item?._id}`} key={i} className="w-full sm:w-6/12 md:w-4/12 lg:w-3/12 px-2.5 mb-5" >
+                                    <div className="bg-[#FFE8E8] rounded-[10px] px-4 lg:px-6 py-5 lg:py-[30px] min-h-full">
+                                        <div className="h-[134px] w-[134px] lg:h-[164px] lg:w-[164px] rounded-full overflow-hidden border-[3px] border-[rgba(56,121,117,0.2)] border-solid mx-auto mb-3 lg:mb-5 ">
+                                            <Image src={item?.userId?.profile_photo || TeacherImg} alt={item?.userId?.name} width={164} height={164} />
+                                        </div>
+                                        <h3 className="text-black text-base font-bold -tracking-[0.03em] text-center mb-2.5 lg:mb-3">{item?.userId?.name}</h3>
+                                        <p className="text-[#535353] text-base font-medium -tracking-[0.03em] mb-4 lg:mb-5 line-clamp-2 text-center">{item?.description}</p>
+                                        <div className="flex flex-wrap gap-3 justify-center">
+                                            <span className="text-[#CC2828] -tracking-[0.03em] font-bold text-base">
+                                                {item?.average_price && item?.average_duration && (
+                                                    `${formatMultiPrice(item?.average_price, "USD")}/${item?.average_duration} min`
+                                                )}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))
+                        }
+                    </div>}
+                {/* {loading ?
                     <BookLoader />
                     :
                     <div className="flex flex-wrap justify-center ">
@@ -44,7 +69,7 @@ export default function Teacher({ teacherData, loading }) {
                                 </Link>
                             ))
                         }
-                    </div>}
+                    </div>} */}
             </div>
         </div>
     )

6f01cc11778ffec04b74c300c52ebc52ff7a6b29
sunil-sainiibs37 <sunil.saini@internetbusinesssolutionsindia.com>
2025-06-13 16:21:35 +0530

    code responsive

--- src/pages/teacher-dashboard/earnings/index.jsx
+++ src/pages/teacher-dashboard/earnings/index.jsx
@@ -115,14 +115,14 @@ export default function index() {
             </h2> */}
             <div className="w-1/3 max-w-sm relative">
               <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <FiSearch className="text-[#CC2828]" />
+                <FiSearch className="text-[#888]" />
               </span>
               <input
                 type="text"
                 value={searchText}
                 onChange={handleSearchChange}
                 placeholder="Search using name or payment id"
-                className="w-full pl-10 pr-4 py-2 border border-[#CC2828] text-[#CC2828] rounded focus:outline-none focus:ring-2 focus:ring-[#CC2828] placeholder-gray-400"
+                className="w-full pl-10 pr-4 py-2 border border-[#ddd] text-[#000] rounded focus:outline-none focus:ring-2 focus:ring-[#CC2828] placeholder-gray-400"
               />
             </div>
             <div className="flex items-center space-x-3">

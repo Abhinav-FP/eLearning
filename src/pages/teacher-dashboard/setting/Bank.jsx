77c8e79b656682227981eb933bafd435db3ca370
sunil-sainiibs37 <sunil.saini@internetbusinesssolutionsindia.com>
2025-06-17 12:17:18 +0530

    teacher pannl

--- src/pages/teacher-dashboard/setting/Bank.jsx
+++ src/pages/teacher-dashboard/setting/Bank.jsx
@@ -241,7 +241,7 @@ export default function Bank() {
             <div className="flex justify-center gap-4 pt-6">
                 <button
                     type="button"
-                    className="w-full max-w-[183px] bg-[#CC2828] hover:bg-red-700 text-white py-2.5 
+                    className="w-full max-w-[183px] bg-[#CC2828] hover:bg-red-700 text-white py-2.5 lg:py-3.5
                     rounded-[10px] text-base xl:text-xl cursor-pointer"
                     onClick={() => setShowDetails(!showDetails)}
                 >
@@ -250,7 +250,7 @@ export default function Bank() {
                 <button
                     type="submit"
                     className={`w-full max-w-[183px] ${!showDetails ? "cursor-not-allowed" : "Cursor-pointer"} 
-                        bg-[#CC2828] hover:bg-red-700 text-white py-2.5 rounded-[10px] text-base xl:text-xl`}
+                        bg-[#CC2828] hover:bg-red-700 text-white py-2.5 lg:py-3.5 rounded-[10px] text-base xl:text-xl`}
                     disabled={processing || !showDetails}
                 >
                     {processing ? "Submitting..." : "Submit"}

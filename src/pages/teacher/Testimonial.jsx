611a87f2679b803ff8f53272be8cab27a8049fb3
Abhinav-FP <a.mathur@futureprofilez.com>
2025-07-03 10:58:20 +0530

    Updated

--- src/pages/teacher/Testimonial.jsx
+++ src/pages/teacher/Testimonial.jsx
@@ -9,20 +9,33 @@ import 'swiper/css/autoplay';
 import Image from "next/image";
 import TeacherImg from "../Assets/Images/teacherimg01.png";
 import StarRating from "../common/StarRating";
+import NoData from "../common/NoData";
 export default function Testimonial({ reviews }) {
 
     return (
-        <>
-            <div className="pt-[20px] lg:pt-[40px] xl:pt-[60px] pb-[40px] lg:pb-[60px] bg-[#F8F9FA]">
-                <div className="container sm:container md:container lg:container xl:max-w-[1230px]  bg-[rgba(249,190,191, .1)] px-4 mx-auto">
-                    <Heading classess={'text-[#1E1E1E] mb-3 lg:mb-4 text-center'} title={'Reviews'} />
-                    <p className="text-[#4E4E4E] tracking-[-0.04em] text-base mb-6 text-center lg:mb-10">Hear from our educators about their journey and experiences with Japanese for Me</p>
+        <div className="pt-[20px] lg:pt-[40px] xl:pt-[60px] pb-[40px] lg:pb-[60px] bg-[#F8F9FA]">
+                  <div className="container sm:container md:container lg:container xl:max-w-[1230px] bg-[rgba(249,190,191, .1)] px-4 mx-auto">
+                <Heading classess={'text-[#1E1E1E] mb-3 lg:mb-4 text-center'} title={'Reviews'} />
+
+                {!reviews || reviews.length === 0 ? (
+                    // <p className="text-center text-gray-500 text-lg font-medium py-12">No reviews available</p>
+                    <NoData
+                        Heading={"No Reviews found"}
+                        content={
+                            "This profile does not have any reviews at the moment. The reviews will appear once approved."
+                        }
+                    />
+                ) : (
+              <>
+                <p className="text-[#4E4E4E] tracking-[-0.04em] text-base mb-6 text-center lg:mb-10">
+                    Hear from our educators about their journey and experiences with Japanese for Me
+                </p>
                     <Swiper
                         modules={[Navigation, Pagination, Autoplay]}
                         spaceBetween={20}
                         slidesPerView={3}
                         autoplay={{
-                            delay: 3000,     
+                            delay: 3000,
                             disableOnInteraction: false,
                         }}
                         navigation
@@ -40,30 +53,33 @@ export default function Testimonial({ reviews }) {
                             },
                         }}
                     >
-                        {
-                            reviews && reviews?.map((item, index) => (
-                                <SwiperSlide key={index}>
-                                    <div className="border border-[rgba(56,121,117,0.2)] rounded-[20px] p-[30px] lg:p-[40px]">
-                                        <div className="w-[42px] h-[60px] lg:w-[42px] lg:w-[60px] mx-auto mb-3 lg:mb-4">
-                                            <Image
-                                                src={item?.userId?.profile_photo || TeacherImg}
-                                                alt={item?.userId?.name || "Profile"}
-                                                width={42}
-                                                height={60}
-                                            />
-                                        </div>
-                                        <h3 className="text-black text-center -tracking-[0.03em] text-base font-semibold mb-3">{item?.userId?.name}</h3>
-                                        <div className="flex text-[#E4B750] text-sm justify-center mb-4">
-                                            <StarRating rating={item?.rating} />
-                                        </div>
-                                        <p className="text-[#535353] font-medium italic text-base text-center">{item?.description}</p>
+                        {reviews.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="border border-[rgba(56,121,117,0.2)] rounded-[20px] p-[30px] lg:p-[40px]">
+                                    <div className="w-[42px] h-[60px] mx-auto mb-3 lg:mb-4">
+                                        <Image
+                                            src={item?.userId?.profile_photo || TeacherImg}
+                                            alt={item?.userId?.name || "Profile"}
+                                            width={42}
+                                            height={60}
+                                        />
                                     </div>
-                                </SwiperSlide>
-                            ))
-                        }
+                                    <h3 className="text-black text-center -tracking-[0.03em] text-base font-semibold mb-3">
+                                        {item?.userId?.name}
+                                    </h3>
+                                    <div className="flex text-[#E4B750] text-sm justify-center mb-4">
+                                        <StarRating rating={item?.rating} />
+                                    </div>
+                                    <p className="text-[#535353] font-medium italic text-base text-center">
+                                        {item?.description}
+                                    </p>
+                                </div>
+                            </SwiperSlide>
+                        ))}
                     </Swiper>
+                    </>
+                )}
                 </div>
-            </div>
-        </>
-    )
+        </div>
+    );
 }

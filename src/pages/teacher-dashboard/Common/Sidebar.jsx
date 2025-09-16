2ea88f49d70f9e2fb73ae9a57477854085b8a5db
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-09-16 17:45:45 +0530

    Showing message count

--- src/pages/teacher-dashboard/Common/Sidebar.jsx
+++ src/pages/teacher-dashboard/Common/Sidebar.jsx
@@ -90,7 +90,7 @@ function SideBar() {
                             </Link>
                             <Link
                                 href="/teacher-dashboard/message"
-                                className={`flex items-center py-2.5 px-3 md:px-4 lg:px-6 gap-2 text-[#565F66] text-base font-medium tracking-[-0.06em] ${pathname === "/teacher-dashboard/message" ? "text-white bg-[#D6202C]" : "hover:bg-gray-100"}`}
+                                className={`relative flex items-center py-2.5 px-3 md:px-4 lg:px-6 gap-2 text-[#565F66] text-base font-medium tracking-[-0.06em] ${pathname === "/teacher-dashboard/message" ? "text-white bg-[#D6202C]" : "hover:bg-gray-100"}`}
                             >
                                 <LuMessagesSquare size={20} />
                                 Messages

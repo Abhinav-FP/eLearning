283513bda0276fc16cd995c9348a50fd7ddade38
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-12-16 15:39:31 +0530

    Updated captcha code 5

--- next.config.mjs
+++ next.config.mjs
@@ -23,32 +23,32 @@ const nextConfig = {
   env:{
     NEXT_PUBLIC_PAYPAL_CLIENT_ID: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
   },
-  async headers() {
-  return [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "Content-Security-Policy",
-          value:
-            "frame-ancestors 'self' https://challenges.cloudflare.com; " +
-            "font-src 'self' https://challenges.cloudflare.com;",
-        },
+//   async headers() {
+//   return [
+//     {
+//       source: "/(.*)",
+//       headers: [
+//         {
+//           key: "Content-Security-Policy",
+//           value:
+//             "frame-ancestors 'self' https://challenges.cloudflare.com; " +
+//             "font-src 'self' https://challenges.cloudflare.com;",
+//         },
 
-        // ❌ REMOVE THIS — it breaks Turnstile
-        // {
-        //   key: "X-Frame-Options",
-        //   value: "DENY",
-        // },
+//         // ❌ REMOVE THIS — it breaks Turnstile
+//         // {
+//         //   key: "X-Frame-Options",
+//         //   value: "DENY",
+//         // },
 
-        { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains" },
-        { key: "X-Content-Type-Options", value: "nosniff" },
-        { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-        { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
-      ],
-    },
-  ];
-},
+//         { key: "Strict-Transport-Security", value: "max-age=31536000; includeSubDomains" },
+//         { key: "X-Content-Type-Options", value: "nosniff" },
+//         { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+//         { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
+//       ],
+//     },
+//   ];
+// },
 };
 
 export default nextConfig;

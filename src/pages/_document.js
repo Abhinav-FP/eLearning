f3053d47712c7f066efe26f83526611cecd292df
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2025-12-16 16:22:53 +0530

    Updated captcha code 7

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,20 +1,19 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Standard favicon */}
         <link rel="icon" href="/favicon.png" />
-        <script
-          src="https://challenges.cloudflare.com/turnstile/v0/api.js"
-          async
-          defer
-        />
       </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
+        <Script
+          src="https://challenges.cloudflare.com/turnstile/v0/api.js"
+          strategy="afterInteractive"
+        />
       </body>
     </Html>
   );
